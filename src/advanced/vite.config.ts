820a66acc5dd2e806b8190fae959c3e17b8d9d90
yuhyeon99 <jujini31@gmail.com>
2025-08-01 00:14:46 +0900

    feat: vite.config.ts 파일에 base 경로를 추가

--- src/advanced/vite.config.ts
+++ src/advanced/vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/front_6th_chapter2-1/',
   plugins: [react(), tailwindcss()],
 })
